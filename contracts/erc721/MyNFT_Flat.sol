639d224f28e9759d4e8659c28713724c8b735a68
kakanda007 <52395903+kakanda007@users.noreply.github.com>
2022-02-22 22:43:17 +0700

    Update MyNFT_Flat.sol

--- contracts/erc721/MyNFT_Flat.sol
+++ contracts/erc721/MyNFT_Flat.sol
@@ -1232,7 +1232,7 @@ abstract contract Ownable is Context {
 
 pragma solidity >=0.7.0 <0.9.0;
 
-contract NFT is ERC721Enumerable, Ownable {
+contract kakanda is ERC721Enumerable, Ownable {
   using Strings for uint256;
 
   string baseURI;
